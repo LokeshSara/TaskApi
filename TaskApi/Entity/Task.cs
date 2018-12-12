b49b32091716f9655f84c636b75ac6032e49bf33
=LSaravananH <=lokesh_saravanan@hotmail.com>
2018-12-12 16:49:30 -0600

    check in

---
+++ TaskApi/Entity/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Entity
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        public int ParentId { get; set; }
+        public string TaskDesc { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Priority { get; set; }
+    }
+}
---
+++ TaskApi/Entity/TaskDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Entity
+{
+    public class TaskDBContext : DbContext
+    {
+        DbSet<Task> Tasks { get; set; }
+
+        DbSet<ParentTask> ParentTask { get; set; }
+    }
+}
